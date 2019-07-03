2ca40b8cc3892a8ac8e990c07530bc1db892aeb5
tavisca-brai <brai@tavisca.in>
2019-07-03 13:30:56 +0530

    FixedMultiplication

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -2,15 +2,18 @@
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
-    class Program
+    class FixMultiplication
     {
         static void Main(string[] args)
         {
+            Test("2?*10=200", 0);
             Test("42*47=1?74", 9);
             Test("4?*47=1974", 2);
             Test("42*?7=1974", 4);
             Test("42*?47=1974", -1);
             Test("2*12?=247", -1);
+            Test("42*47=1??4",-1);
+          
             Console.ReadKey(true);
         }
 
@@ -23,7 +26,57 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
+            int result = -1;
+
+            string[] str = equation.Split(new char[] { '*', '=' });
+            string str1 = str[0];
+            string str2 = str[1];
+            string str3 = str[2];
+
+            int tmpresult = 0;
+            if(str1.Contains('?'))
+            {
+                int c = int.Parse(str3);
+                int b = int.Parse(str2);
+                if(c%b==0)
+                {
+                    tmpresult = c / b;
+                    result = checkIndex(tmpresult, str1);
+                }
+            }
+            else if (str2.Contains('?'))
+            {
+                int c = int.Parse(str3);
+                int a = int.Parse(str1);
+                if (c % a == 0)
+                {
+                    tmpresult = c / a;
+                    result = checkIndex(tmpresult, str2);
+                }
+            }
+            else if (str3.Contains('?'))
+            {
+                tmpresult = int.Parse(str1) * int.Parse(str2);
+                result = checkIndex(tmpresult, str3);
+            }
+
+            return result;
             throw new NotImplementedException();
+        }
+
+        public static int checkIndex(int tmpres, string str)
+        {
+            int res = -1;
+
+            if ((str.Split('?').Length - 1) > 1) return -1;
+
+            int ind = str.IndexOf('?');
+            string strRes = tmpres.ToString();
+            if (str.Length == strRes.Length)
+            {
+                res = strRes[ind] - '0';
+            }
+            return res;
         }
     }
 }
